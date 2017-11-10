da720e658a96f225ce019c68661e8a93301cc021
Tarcisio Sassara <sassara.tarcisio@gmail.com>
2017-11-10 16:56:23 -0200

    license

--- node/storage/postgres/sql.go
+++ node/storage/postgres/sql.go
@@ -1,3 +1,7 @@
+// Copyright 2017. All rights reserved.
+// Use of this source code is governed by a BSD 3-Clause License
+// license that can be found in the LICENSE file.
+
 package postgres
 
 const sqlInsert = "insert into vinodes.nodes(id, parent, lft, rgt) values ($1, $2, $3, $4)"
